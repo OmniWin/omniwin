a5f7d03a0264e9d94334ec17b1725fc1cb3a24dc
martyaxel <buciumarian90@gmail.com>
2024-01-26 22:51:06 +0200

    x

--- backend/api/src/repositories/nftRepository.ts
+++ backend/api/src/repositories/nftRepository.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify';
+// import { FetchNFTsResultType } from "../types/fetchNfts";
 
 export class NftRepository {
     fastify: FastifyInstance;
@@ -6,18 +7,103 @@ export class NftRepository {
         this.fastify = fastify;
     }
 
-    async fetchNFTs(whereCondition: any, limit: number, orderBy: any) {
+    async fetchNFTs(whereCondition: any, limit: number, orderBy: any): Promise<{
+        id_nft: number;
+        id_lot: number;
+        total_tickets: number;
+        bonus_tickets: number;
+        tickets_bought: number;
+        ticket_price: number;
+        transactions: number;
+        end_timestamp: Date;
+        fee: number;
+        closed: boolean;
+        buyout: number;
+        asset_claimed: boolean;
+        tokens_claimed: boolean;
+        owner: string;
+        signer: string;
+        token: string;
+        token_id: string;
+        amount: number;
+        asset_type: string;
+        data: string;
+        network: string;
+        created_at: Date;
+        updated_at: Date;
+        name: string;
+        image: string;
+
+    }[]> {
         const { prisma } = this.fastify;
 
-        const nfts = await prisma.nft.findMany({
-            where: whereCondition,
-            take: limit + 1, // Fetch one extra item to determine if there's a next page
-            orderBy: orderBy,
-            include: {
-                metadata: true,
-            }
-        });
+        // Construct the base query with parameter placeholders
+        let rawQuery = `SELECT id_nft, id_lot, total_tickets, bonus_tickets, tickets_bought, ticket_price, transactions, end_timestamp, fee, closed, buyout, asset_claimed, tokens_claimed, owner, signer, token, token_id, amount, asset_type, data, network, created_at, updated_at, NftMetadata.name, NftMetadata.image FROM Nft
+        LEFT JOIN NftMetadata ON Nft.id_nft = NftMetadata.id_nft
+        `;
 
-        return nfts;
+        // Parameters for the query
+        let queryParams: any[] = [];
+
+        // Building the WHERE clause with parameters
+        let whereClauses = [];
+
+        if (whereCondition.asset_type?.in) {
+            whereClauses.push(`asset_type IN (${whereCondition.asset_type.in.map(() => '?').join(', ')})`);
+            queryParams.push(...whereCondition.asset_type.in);
+        }
+
+        if (whereCondition.network?.in) {
+            whereClauses.push(`network IN (${whereCondition.network.in.map(() => '?').join(', ')})`);
+            queryParams.push(...whereCondition.network.in);
+        }
+
+        if (whereClauses.length > 0) {
+            rawQuery += ` WHERE ${whereClauses.join(' AND ')}`;
+        }
+
+        if (orderBy.custom === 'PriceHighToLow') {
+            // Adding ORDER BY and LIMIT with placeholders
+            rawQuery += ` ORDER BY (ticket_price / 6) * total_tickets DESC LIMIT ?`;
+        }
+
+        if (orderBy.custom === 'PriceLowToHigh') {
+            // Adding ORDER BY and LIMIT with placeholders
+            rawQuery += ` ORDER BY (ticket_price / 6) * total_tickets ASC LIMIT ?`;
+        }
+
+        if (orderBy.custom === 'TicketsRemaining') {
+            // Adding ORDER BY and LIMIT with placeholders
+            rawQuery += ` ORDER BY (total_tickets - tickets_bought) DESC LIMIT ?`;
+        }
+
+        if (orderBy.custom === 'Newest') {
+            // Adding ORDER BY and LIMIT with placeholders
+            rawQuery += ` ORDER BY created_at DESC LIMIT ?`;
+        }
+
+        if (orderBy.custom === 'Oldest') {
+            // Adding ORDER BY and LIMIT with placeholders
+            rawQuery += ` ORDER BY created_at ASC LIMIT ?`;
+        }
+
+        if (orderBy.custom === 'TimeRemaining') {
+            // Adding ORDER BY and LIMIT with placeholders
+            rawQuery += ` ORDER BY end_timestamp ASC LIMIT ?`;
+        }
+
+
+        queryParams.push(limit + 1);
+
+
+        console.log("rawQuery", rawQuery);
+        // Execute the query with parameterized values
+        const nfts = await prisma.$queryRawUnsafe(rawQuery, ...queryParams);
+
+
+
+        return nfts as any[];
+
     }
+
 }
--- backend/api/src/types/fetchNfts.ts
+++ backend/api/src/types/fetchNfts.ts
@@ -1,4 +1,24 @@
-import { NftRepository } from "../repositories/nftRepository";
+// import { NftRepository } from "../repositories/nftRepository";
 
-type ResolvedType<T> = T extends Promise<infer R> ? R : T;
-export type FetchNFTsResultType = ResolvedType<ReturnType<InstanceType<typeof NftRepository>['fetchNFTs']>>;
+// type ResolvedType<T> = T extends Promise<infer R> ? R : T;
+// export type FetchNFTsResultType = ResolvedType<ReturnType<InstanceType<typeof NftRepository>['fetchNFTs']>>;
+
+import { AssetType, NetworkType } from "@prisma/client";
+
+export type FetchNFTsResultType = {
+    items: {
+        full_price: number;
+        ticket_price: number;
+        tickets_bought: number;
+        tickets_total: number;
+        time_left: Date;
+        nft_name: string;
+        nft_image: string;
+        nft_owner: string;
+        asset_type: AssetType;
+        nft_id: number;
+        token_id: string;
+        network: NetworkType;
+    }[],
+    nextCursor: string | null;
+}
